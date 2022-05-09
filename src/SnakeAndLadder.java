b5ede7df36e1dd70c620c56188e0e45312e4bd93
Ritwik <deshrutu@gmail.com>
2022-05-09 17:06:34 +0530

    UC1 - Player Start At 0

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,6 +1,54 @@
 public class SnakeAndLadder {
+    static int[][] snakePosition = new int[8][2];
+    static int[][] ladderPosition = new int[7][2];
     public static void main(String[] args) {
+        int[][] array = new int[10][10];
+
+        int positionleft = 100;
+        int positionRight = 100;
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0) {
+                positionRight = positionleft;
+                for (int j = 0; j < array[i].length; j++) {
+                    array[i][j] = positionRight--;
+                }
+            } else {
+                positionleft = positionRight;
+                for (int j = array[i].length - 1; j >= 0; j--) {
+                    array[i][j] = positionleft--;
+                }
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        snakePosition[0] = new int[]{40, 3};
+        snakePosition[1] = new int[]{27, 5};
+        snakePosition[2] = new int[]{43, 18};
+        snakePosition[3] = new int[]{54, 31};
+        snakePosition[4] = new int[]{66, 45};
+        snakePosition[5] = new int[]{76, 58};
+        snakePosition[6] = new int[]{89, 53};
+        snakePosition[7] = new int[]{99, 41};
+
+        ladderPosition[0] = new int[]{25, 4};
+        ladderPosition[1] = new int[]{46, 13};
+        ladderPosition[2] = new int[]{49, 33};
+        ladderPosition[3] = new int[]{63, 42};
+        ladderPosition[4] = new int[]{69, 50};
+        ladderPosition[5] = new int[]{81, 62};
+        ladderPosition[6] = new int[]{92, 47};
+
+        int playerPosition = 0;
+        boolean isGameRunning = true;
+
         System.out.println("Snake And Ladder Game");
         System.out.println("---------------------");
+        System.out.println("Player Starting Position: " + playerPosition);
     }
 }
