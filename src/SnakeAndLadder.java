bd6018c2f8f52be19a338e458825055c85fdb11c
Ritwik <deshrutu@gmail.com>
2022-05-09 17:01:18 +0530

    Snake and Ladder Problem

---
+++ src/SnakeAndLadder.java
@@ -0,0 +1,6 @@
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Snake And Ladder Game");
+        System.out.println("---------------------");
+    }
+}
